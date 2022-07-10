d5e845edeae1cc74c01663dd36c1e38785f7e55c
Sehaj <sehajkalra751999@gmail.com>
2022-07-10 10:52:09 -0400

    updated UI

--- Recoil/Assets/Scripts/InGameUIUpdate.cs
+++ Recoil/Assets/Scripts/InGameUIUpdate.cs
@@ -16,6 +16,6 @@ public class InGameUIUpdate : MonoBehaviour
 
     void SetBulletCountText()
     {
-        bulletCountText.text = player.BulletCount.ToString() + "/" + player.MaxBulletCount.ToString();
+        bulletCountText.text = player.BulletCount.ToString();
     }
 }
