5da1eb117a5305e1511e12816bd1fa65d79bb927
Sehaj <sehajkalra751999@gmail.com>
2022-07-10 22:27:41 -0400

    adjusted canvas to scale with screen size

--- Recoil/Assets/Scripts/MainMenu.cs
+++ Recoil/Assets/Scripts/MainMenu.cs
@@ -5,6 +5,6 @@ public class MainMenu : MonoBehaviour
 {
     public void Play()
     {
-        SceneManager.LoadScene("Main");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
