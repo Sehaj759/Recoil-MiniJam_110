d277dab32bfaa4629f029ed8362a00a3430403ad
Sehaj <sehajkalra751999@gmail.com>
2022-07-10 23:28:30 -0400

    added ammo pack visual on instruction screen

--- Recoil/Assets/Scripts/MainMenu.cs
+++ Recoil/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Image ammoPack;
+
+    void Update()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            ammoPack.color = Color.HSVToRGB(Mathf.PingPong(Time.time, 1), 1, 1);
+        }    
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
